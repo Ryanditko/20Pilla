b56854d8799ba1d6f7042e24cbaa4a6f5c29498d
Ryan Rodrigues Cordeiro <yryurodriguess@gmail.com>
2025-07-04 19:12:44 -0300

    Add files via upload

---
+++ Landing_Page_20Pilla/20Pilla/Landing_Page_20Pilla/src/javascript/google_scripts.js
@@ -0,0 +1,57 @@
+document.getElementById("newsletter-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const url = "https://script.google.com/macros/s/AKfycbyiRHncp5eYlofNdpfDdr6m2jZaW4hWgTDcT4MSc8qtSkt-LzSDn_jKMt_ecOnmHl-u/exec" + encodeURIComponent(email);
+
+
+  const formData = new URLSearchParams();
+  formData.append("email", email);
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: formData.toString()
+  })
+  .then(response => response.text())
+  .then(text => {
+    if (text.toLowerCase().includes("sucesso")) {
+      alert("🎉 Cadastro realizado com sucesso!");
+      document.getElementById("newsletter-form").reset();
+    } else {
+      alert("⚠️ Algo deu errado: " + text);
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    alert("⚠️ Ocorreu um erro. Tente novamente.");
+  });
+});
+
+//function doPost(e) {
+    //var sheet = SpreadsheetApp.openById("1AffemlcmQDTGBuqNPqGYhJNJJwzsWAXsRovBOXFX4P4").getSheetByName("Página1");//
+
+   // var email = e.parameter.email;
+   // var data = new Date();//
+  
+   // sheet.appendRow([email, data]);//
+  
+    //return ContentService.createTextOutput("Cadastro realizado com sucesso!");
+  //}//
+  
+  //function enviarEmails() {
+    // var sheet = SpreadsheetApp.openById("1AffemlcmQDTGBuqNPqGYhJNJJwzsWAXsRovBOXFX4P4").getSheetByName("Página1");
+    // var dados = sheet.getRange("A2:A" + sheet.getLastRow()).getValues();
+    // var assunto = "Novidades da 20Pilla!";
+    // var mensagem = "Olá! Aqui estão as últimas novidades. Fique ligado! 💜";//
+  
+    //dados.forEach(function(linha) {
+    //   var email = linha[0];
+    //   if (email) {
+        // MailApp.sendEmail(email, assunto, mensagem);
+    //   }
+    // });//
+  //} //
+  
---
+++ Landing_Page_20Pilla/20Pilla/README.md
@@ -0,0 +1,11 @@
+#  20Pilla | Landing Page
+
+<ul>
+    <li>Roxo para a ousadia.</li>
+    <li>Preto para a presença.</li>
+    <li>Branco para a essência.</li>
+</ul>
+
+hospedagem: https://20-pilla.vercel.app
+
+<img src= "https://i.imgur.com/gFJLi2V.jpeg" alt="20Pilla Landing Page" width="600">
